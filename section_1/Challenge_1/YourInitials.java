760a21fbe9db95568d5ef72ca38f1ac9c0492ee3
Salvador Montiel <salvoperez51@gmail.com>
2024-06-06 15:29:24 -0600

    Files test

---
+++ section_1/Challenge_1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println(" SSS   M   M");
+         System.out.println("S   S  MM MM");
+         System.out.println("S      M M M");
+         System.out.println(" SSS   M   M");
+         System.out.println("    S  M   M");
+         System.out.println("S   S  M   M");
+         System.out.println(" SSS   M   M");
+    }
+}
