fead6e7f360ba4d656e1543afc458640d18c0439
Salvador Montiel <salvoperez51@gmail.com>
2024-06-07 13:08:11 -0600

    Finish section 2

---
+++ section_2/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args){
+        long globalPopulation = 8_000_000_000L;
+        long dailyGoogleSearches = 85_000_000_000L;
+
+        System.out.println("The global population is " + globalPopulation);
+        System.out.println("Daily, there are " + dailyGoogleSearches + " google searches.");
+    }
+}
